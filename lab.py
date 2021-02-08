4bb3248c2d6716942ceedcda5c0d6e328385267c
PreranaPandit <preranapandit07@gmail.com>
2021-02-08 10:35:31 +0545

    Lab Exercises

---
+++ lab.py
@@ -0,0 +1,7 @@
+#1.write a program that takes three numbers and prints their sum.Every number is given on separate lines.
+
+a=int(input("enter the first value:"))
+b=int(input("enter the second value:"))
+c=int(input("enter the third value:"))
+d=a+b+c
+print(f"the sum of given three numbers is {d}")
